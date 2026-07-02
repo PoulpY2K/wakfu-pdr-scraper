2d3e9c9067041c79875ebe591c508762febcf7ea
poulpy2k <contact@jeremy-laurent.com>
2026-07-02 15:25:40 +0200

    fix: working tests with application-test.yaml

--- src/test/java/fr/fumbus/wakfupdrscraper/WakfuPdrScraperApplicationTests.java
+++ src/test/java/fr/fumbus/wakfupdrscraper/WakfuPdrScraperApplicationTests.java
@@ -1,13 +1,23 @@
 package fr.fumbus.wakfupdrscraper;
 
+import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
+@RequiredArgsConstructor
+@ActiveProfiles({"test"})
 class WakfuPdrScraperApplicationTests {
+
+    private final ApplicationContext applicationContext;
 
     @Test
     void contextLoads() {
+        assertNotNull(applicationContext);
     }
 
 }
